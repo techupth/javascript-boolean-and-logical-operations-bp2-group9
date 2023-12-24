d660df85280098f98bd6b55dffa01378325a3c8d
Worawat <kimcoop2561@gmail.com>
2023-12-25 00:47:00 +0700

    feat(exercise) : add completed Boolean and Logical Operations

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,10 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
 
 // Start coding here
+let isOver18 = true; 
+let hasCriminalBlacklist = true; 
+let isAllow = true; 
+isOver18 = true; 
+hasCriminalBlacklist = false; 
+isAllow = isOver18 && hasCriminalBlacklist; 
 
-console.log(isAllow);
+console.log(isAllow); //false
